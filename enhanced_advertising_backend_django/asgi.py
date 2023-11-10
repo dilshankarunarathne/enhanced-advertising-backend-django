33fed154e37caccd620f6ad4c594b0b68f254900
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-11-10 22:34:37 +0530

    Update asgi.py

--- enhanced_advertising_backend_django/asgi.py
+++ enhanced_advertising_backend_django/asgi.py
@@ -11,6 +11,16 @@ import os
 
 from django.core.asgi import get_asgi_application
 
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django.urls import path
+
+from enhanced_advertising_backend_django.routing import websocket_urlpatterns
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'enhanced_advertising_backend_django.settings')
 
-application = get_asgi_application()
+
+application = ProtocolTypeRouter({
+    "websocket": URLRouter(
+        websocket_urlpatterns
+    ),
+})
