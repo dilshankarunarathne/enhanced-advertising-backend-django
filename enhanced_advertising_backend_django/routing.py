40d34e550713af7beae2d4471cf3781b14201b69
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-11-10 22:32:25 +0530

    Create routing.py

---
+++ enhanced_advertising_backend_django/routing.py
@@ -0,0 +1,10 @@
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django.urls import path
+
+from enhanced_advertising_backend_django.consumers import VideoConsumer
+
+application = ProtocolTypeRouter({
+    "websocket": URLRouter([
+        path("ws/video/", VideoConsumer.as_asgi()),
+    ]),
+})
