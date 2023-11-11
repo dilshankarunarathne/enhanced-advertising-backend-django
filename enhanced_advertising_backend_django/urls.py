47eb0e15c6c4d61651c18763686a9a2c44e389a9
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-11-11 11:56:36 +0530

    Update urls.py

--- enhanced_advertising_backend_django/urls.py
+++ enhanced_advertising_backend_django/urls.py
@@ -5,3 +5,7 @@ from . import views
 urlpatterns = [
     path('ws/video_stream/', views.video_stream, name='video_stream'),
 ]
+
+websocket_urlpatterns = [
+    re_path(r'ws/video_stream/$', consumers.VideoStreamConsumer.as_asgi()),
+]
