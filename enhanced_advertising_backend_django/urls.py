041c35ddd25caa36b967c90ca871ec8f3fdb1980
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-11-11 11:59:45 +0530

    Update urls.py

--- enhanced_advertising_backend_django/urls.py
+++ enhanced_advertising_backend_django/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path
-from . import views
+from . import views, consumers
+from django.urls import re_path
 
 urlpatterns = [
     path('ws/video_stream/', views.video_stream, name='video_stream'),
