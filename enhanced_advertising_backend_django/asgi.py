a530930fc415e9cb9d81c97e4f81286852aaf9df
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-11-11 11:29:14 +0530

    Update asgi.py

--- enhanced_advertising_backend_django/asgi.py
+++ enhanced_advertising_backend_django/asgi.py
@@ -1,12 +1,3 @@
-"""
-ASGI config for enhanced_advertising_backend_django project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
-"""
-
 import os
 
 from django.core.asgi import get_asgi_application
