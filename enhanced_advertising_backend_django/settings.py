c6266e54ad535fd9e7ffad714e20f1e0f1410179
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-11-11 12:02:57 +0530

    Update settings.py

--- enhanced_advertising_backend_django/settings.py
+++ enhanced_advertising_backend_django/settings.py
@@ -1,5 +1,7 @@
 from pathlib import Path
+from django.conf import settings
 
+default_channel_layer = settings.DEFAULT_CHANNEL_LAYER
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
