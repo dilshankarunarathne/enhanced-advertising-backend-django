60076430cc11568af608cbe21f213da82fcf0a46
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-11-11 11:30:10 +0530

    Update settings.py

--- enhanced_advertising_backend_django/settings.py
+++ enhanced_advertising_backend_django/settings.py
@@ -4,9 +4,6 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-h4@^=b#rpqy2ps89q6(ct19bwm&j-f=$j740+e_h^ctdl^@(w0'
 
