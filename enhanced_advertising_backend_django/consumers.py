aacb2ee8e79a303f9f6b8cd40439911b8c2f03ee
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-11-12 17:22:49 +0530

    Update consumers.py

--- enhanced_advertising_backend_django/consumers.py
+++ enhanced_advertising_backend_django/consumers.py
@@ -7,6 +7,7 @@ import numpy as np
 from channels.generic.websocket import AsyncWebsocketConsumer
 
 from enhanced_advertising_backend_django import classifier
+from enhanced_advertising_backend_django.recommender import predict_interest
 
 
 class VideoStreamConsumer(AsyncWebsocketConsumer):
