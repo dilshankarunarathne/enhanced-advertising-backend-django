bb5afe71f6529ad2f6350fab440f0e4a21329af4
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-11-13 10:22:02 +0530

    stream connected to model

--- enhanced_advertising_backend_django/classifier.py
+++ enhanced_advertising_backend_django/classifier.py
@@ -16,12 +16,16 @@ gender_labels = ["Male", "Female"]
 
 
 def predict_age_and_gender(image):
+    if image is None:
+        print(f"Image could not be loaded.")
+    else:
+        print(f"Image loaded successfully.")
     blob = cv2.dnn.blobFromImage(
         image,
         1.0,
         (227, 227),
         (78.4263377603, 87.7689143744, 114.895847746),
-        swapRB=False
+        swapRB=True
     )
 
     age_net.setInput(blob)
--- enhanced_advertising_backend_django/consumers.py
+++ enhanced_advertising_backend_django/consumers.py
@@ -3,6 +3,7 @@ import json
 import base64
 import cv2
 import numpy as np
+import re
 
 from channels.generic.websocket import AsyncWebsocketConsumer
 
@@ -19,14 +20,33 @@ class VideoStreamConsumer(AsyncWebsocketConsumer):
         pass
 
     async def receive(self, text_data):
-        frame_data = base64.b64decode(text_data)
+        # Extract base64 string from data URL
+        base64_str = re.search(r'base64,(.*)', text_data).group(1)
+        frame_data = base64.b64decode(base64_str)
         nparr = np.frombuffer(frame_data, np.uint8)
         frame = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+        if frame is None:
+            print("error frame is none")
+        else:
+            print("Image decoded successfully")
         model_output = model_processing(frame)
-        await self.send(text_data=json.dumps({model_output}))
+        await self.send(text_data=json.dumps({"model_output": model_output}))
+
+
+
+def check_base64_image(base64_string, output_file):
+    try:
+        with open(output_file, 'wb') as f_out:
+            f_out.write(base64.b64decode(base64_string))
+        print(f"Image written to {output_file}. Please check if it's a valid image.")
+    except Exception as e:
+        print(f"Failed to decode and write image: {e}")
 
 
 def model_processing(frame):
+    if frame is None:
+        print("image empty")
+
     # predict age & gender
     age, gender = classifier.predict_age_and_gender(frame)
 
