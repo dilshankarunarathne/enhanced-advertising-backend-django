49d1f607610660147b5ab17437996e828c30b5dd
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-11-20 11:24:16 +0530

    cors all origins

--- enhanced_advertising_backend_django/settings.py
+++ enhanced_advertising_backend_django/settings.py
@@ -24,6 +24,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'channels',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -34,7 +35,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 CHANNEL_LAYERS = {
     'default': {
